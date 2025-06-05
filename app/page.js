33753fa81b0359ce51a545753cb518f5d04fc76c
andre lucas <andrezaolucas@outlook.com>
2025-06-04 23:35:00 -0300

    agora foi 2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Testando agora</button>
+      <button className="btn">Testando agora2</button>
     </main>
   );
 }
