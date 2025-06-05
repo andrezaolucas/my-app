0522e78587e33f0aebfd792437f152908d787fad
andre lucas <andrezaolucas@outlook.com>
2025-06-04 23:25:52 -0300

    alterando texto btn

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Click me</button>
+      <button className="btn">Testando agora</button>
     </main>
   );
 }
